a2318d5c71fbb0a79511931c62cdacb254e2da84
abouzek <alan.bouzek@gmail.com>
2015-10-05 16:38:33 -0500

    Enable mongoDB Heroku settings

--- __init__.py
+++ __init__.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
 from mox.views import mocks
+import os
 
 app = Flask(__name__)
-app.config["MONGODB_SETTINGS"] = {"DB": "mock_requests"}
+app.config["MONGO"]
+app.config["MONGODB_SETTINGS"] = {
+	"db": "mox"
+}
 app.config["SECRET_KEY"] = "KeepThisS3cr3t"
+
+if os.environ.get('PRODUCTION'):
+	app.config["MONGODB_SETTINGS"]["host"] = os.environ.get("PROD_MONGODB")
 
 db = MongoEngine(app)
 app.register_blueprint(mocks)
